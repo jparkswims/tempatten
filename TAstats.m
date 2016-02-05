ebbda75fa648ef75a6a01bbf81e110c9cfef926b
Jake Parker <jap659@nyu.edu>
2016-02-04 23:41:22 -0500

    added 2nd ANOVA

--- TAstats.m
+++ TAstats.m
@@ -14,5 +14,34 @@ pvalues = ptable(:,1);
 
 sigtimes = find(pvalues < 0.05);
 
-sigtimes2 = sigtimes(diff(sigtimes) == 1);
+dsigtimes = [diff(sigtimes) ; 0];
 
+r = 1;
+i = 1;
+
+while i <= length(dsigtimes)
+    
+    clusters(r,1) = sigtimes(i);
+    
+    while dsigtimes(i) == 1
+        
+        i = i+1;
+        
+    end
+       
+    clusters(r,2) = sigtimes(i);
+    
+    r = r+1;
+    i = i+1;
+    
+end
+
+for i = 1:size(clusters,1)
+    
+    ptable2(i,:) = anova_rm([nanmean(t1det1m(:,clusters(i,1):clusters(i,2)),2) nanmean(neutraldet1m(:,clusters(i,1):clusters(i,2)),2) nanmean(t1det1m(:,clusters(i,1):clusters(i,2)),2)]);
+    
+end
+    
+    
+    
+    
